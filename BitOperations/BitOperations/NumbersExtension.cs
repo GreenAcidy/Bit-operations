aaeec8769d83dc4e3d5b8edcdb75934fb9f5fc29
GreenAcidy <47273458+GreenAcidy@users.noreply.github.com>
2022-10-18 14:03:10 +0300

    Complete 1 task and NUTests

---
+++ BitOperations/BitOperations/NumbersExtension.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="NumbersExtension.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace BitOperations
+{
+    using System;
+
+    /// <summary>
+    /// Class NumberExtensions contains two methods: algorithm of inserting bytes of one number into another; algorithm that check if number is palindrom.
+    /// </summary>
+    internal class NumbersExtension
+    {
+        /// <summary >
+        /// Algorithm of inserting bits of one number into another.
+        /// </summary >
+        /// <param  name = " numberSource " > Number for inserting bits. </param >
+        /// <param  name = " numberIn " > Number from which will get bits. </param >
+        /// <param  name = " firstBit " > First position of inserting bits in number. </param >
+        /// <param  name = " lastBit " > Last position of inserting bits in number. </param >
+        /// <exception cref="ArgumentOutOfRangeException">Throw when index more than 32 or negative.</exception>
+        /// <exception cref="ArgumentException">Throw when firstBit more than lastBit.</exception>
+        /// <returns>Number with inserted bits.  </returns>
+        private static int InsertNumberIntoAnother(int numberSource, int numberIn, int firstBit, int lastBit)
+        {
+            if (firstBit < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(firstBit), "must be positive");
+            }
+
+            if (lastBit < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lastBit), "must be positive");
+            }
+
+            if (lastBit >= 32)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lastBit), "must be less than 32");
+            }
+
+            if (firstBit >= 32)
+            {
+                throw new ArgumentOutOfRangeException(nameof(firstBit), "must be less than 32");
+            }
+
+            if (firstBit > lastBit)
+            {
+                throw new ArgumentException("firstBit must be less than lastBit");
+            }
+
+            int count = lastBit - firstBit;
+            int mask1 = 0;
+            for (int k = count; k >= 0; k--)
+            {
+                mask1 += 1 << k;
+            }
+
+            int temp = mask1 & numberIn;
+            temp <<= firstBit;
+            int mask2 = ~(mask1 << firstBit);
+            numberSource &= mask2;
+            int result = numberSource | temp;
+            return result;
+        }
+    }
+}
---
+++ BitOperations/NumbersExtensionNUTests/NUTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NumbersExtensionNUTests
+{
+    [TestFixture]
+    class NUTests
+    {
+        [TestCase(2728, 655, 3, 8, ExpectedResult = 2680)]
+        [TestCase(554216104, 15, 0, 31, ExpectedResult = 15)]
+        [TestCase(-55465467, 345346, 0, 31, ExpectedResult = 345346)]
+        [TestCase(554216104, 4460559, 11, 18, ExpectedResult = 554203816)]
+        [TestCase(-1, 0, 31, 31, ExpectedResult = 2147483647)]
+        [TestCase(-2147483648, 2147483647, 0, 30, ExpectedResult = -1)]
+        [TestCase(-2223, 5440, 18, 23, ExpectedResult = -16517295)]
+        [TestCase(2147481425, 5440, 18, 23, ExpectedResult = 2130966353)]
+        public int InsertNumberIntoAnotherTests(int sourceNumber, int insertNumber, int i, int j)
+            => NumbersExtension.InsertNumberIntoAnother(sourceNumber, insertNumber, i, j);
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentException() =>
+            Assert.Throws<ArgumentException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, 8, 3),
+                message: "i must be less then j");
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentOutOfRangeException_Less() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, -1, 3),
+                message: "index must be positive");
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentOutOfRangeException_More() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, 32, 32),
+                message: "index must be less then 32");
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentOutOfRangeException_MoreAndLess() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, 0, 32),
+                message: "index must be less then 32");
+
+
+        [TestCase(1001, ExpectedResult = true)]
+        [TestCase(10101, ExpectedResult = true)]
+        [TestCase(10201, ExpectedResult = true)]
+        [TestCase(109901, ExpectedResult = true)]
+        [TestCase(123451001, ExpectedResult = false)]
+
+        public bool IsPalindromeTests(int incomingPalindrome)
+            => NumbersExtension.IsPalindrome(incomingPalindrome);
+
+        [Test]
+        public void NegativeNumberAbsPalindrome_ArgumentOutOfRangeException_Negative() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.IsPalindrome(-1001),
+        message: "number must be positive");
+
+        [Test]
+        public void NegativeNumberAbsFalsePalindrome_ArgumentOutOfRangeException_Negative() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.IsPalindrome(-10123401),
+        message: "number must be positive");
+
+        [Test]
+        [Order(2)]
+        [Timeout(500)]
+        public void PossiblyVerySlowCode_WithTimeLessThan1000Milliseconds()
+        {
+            for (int source = 0; source < 1_000_000; source++)
+            {
+                BitOperations.NumbersExtension.IsPalindrome(source);
+            }
+        }
+
+        [Test]
+        [Order(1)]
+        [Timeout(2_000)]
+        public void PossiblyVerySlowCode_WithTimeLessThan25000Milliseconds()
+        {
+            for (int source = 0; source < 10_000_000; source++)
+            {
+                BitOperations.NumbersExtension.IsPalindrome(source);
+            }
+        }
+    }
+}
